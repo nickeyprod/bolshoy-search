4c85a03ac2d3be32b1f9f5a60ab6be0bf85364e9
Nickeyprod <nickeyprod@gmail.com>
2021-06-23 18:47:45 +0300

    Initial Commit

---
+++ Bolshoi Search/Bolshoi_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bolshoi_SearchApp.swift
+//  Bolshoi Search
+//
+//  Created by Николай Ногин on 23.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Bolshoi_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
